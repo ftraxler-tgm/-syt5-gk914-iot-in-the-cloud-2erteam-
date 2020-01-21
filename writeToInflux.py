cad071182e192fe2323df6ddf65d6cd8f017841c
Karim <komar@student.tgm.ac.at>
2020-01-21 15:49:34 +0100

    Write to AWS-InfluxDB with random test data

--- writeToInflux.py
+++ writeToInflux.py
@@ -1,7 +1,46 @@
 import time
 from influxdb import InfluxDBClient
+import random
 
-client = InfluxDBClient(host='http://ec2-35-159-21-204.eu-central-1.compute.amazonaws.com', port=8086)
+client = InfluxDBClient(host='35.159.21.204', port=8086)
 client.switch_database('sensordaten')
 
-print("BLA")
+while True:
+    humidity = random.uniform(0, 40)
+    temperature = random.uniform(0, 30)
+    light = random.uniform(0, 70)
+    print("Humidity: "+str(humidity))
+    print("Temperature: "+str(temperature))
+    print("Light: "+str(light))
+
+    data = [
+            {
+                "measurement": "humidity",
+                "tags": {
+                    "user": "Omar_Hediyehloo_Traxler"
+                },
+                "fields": {
+                    "value": humidity
+                }
+            }, {
+                "measurement": "temperature",
+                "tags": {
+                    "user": "Omar_Hediyehloo_Traxler"
+                },
+                "fields": {
+                    "value": temperature
+                }
+            }, {
+                "measurement": "light",
+                "tags": {
+                    "user": "Omar_Hediyehloo_Traxler"
+                },
+                "fields": {
+                    "value": light
+                }
+            }
+        ]
+    client.write_points(data)
+    print("Data written!")
+    time.sleep(3)
+
